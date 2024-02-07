e621a6f0e4aca5a78e6497aca88147c418cba900
Hikmal Falah <ikmalfalah@gmail.com>
2024-02-08 04:23:57 +0700

    Update menghapus code yang tidak perlu di post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -14,11 +14,3 @@
         </div>
     </section>
 @endsection
-
-Post::create([
-    'title' => 'Judul ke tiga',
-    'slug' => 'judul-ke-tiga',
-    'author' => 'Dilarba',
-    'excerpt' => 'Lorem ipsum ke tiga.',
-    'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem asperiores nulla vel optio harum iste eaque recusandae blanditiis.</p> <p>Dicta, iste! Animi deleniti adipisci ea id est. Maxime cupiditate optio quisquam, ducimus sapiente numquam fuga laudantium dicta quo tenetur velit repellendus error aliquam distinctio minus placeat quam ut dolor illum consequatur rerum nam.</p> <p>Possimus, quis laboriosam. Labore omnis unde tempora velit, nihil illum mollitia ex a dolorem, natus voluptatibus dolores, veritatis laudantium! Harum, ipsa iure? Debitis ex possimus alias accusantium numquam!</p>'
-])
